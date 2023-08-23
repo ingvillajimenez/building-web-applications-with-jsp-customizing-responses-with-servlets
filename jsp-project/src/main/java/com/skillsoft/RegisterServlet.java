a54f9666360e1624f482330e7e20bafe9f1e9d7e
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-23 15:41:39 -0600

    Processing Inputs in a Servlet

--- jsp-project/src/main/java/com/skillsoft/RegisterServlet.java
+++ jsp-project/src/main/java/com/skillsoft/RegisterServlet.java
@@ -1,4 +1,37 @@
 package com.skillsoft;
 
-public class RegisterServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/register")
+public class RegisterServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+
+        String un = request.getParameter("name");
+        String em = request.getParameter("email");
+        String memb = request.getParameter("membership");
+        String[] subs = request.getParameterValues("subs");
+
+        response.setContentType("text/html");
+        PrintWriter pw  = response.getWriter();
+        pw.write("<br><b>Name: </b>" + un);
+        pw.write("<br><b>Email: </b>" + em);
+        pw.write("<br><b>Membership: </b>" + memb);
+        pw.write("<br><b>Subscription: </b>");
+        for (String sub: subs) {
+            pw.write(sub + " ");
+        }
+    }
 }
+
+// For older Tomcat versions
+// Replace jakarta with javax
--- jsp-project/target/jsp-project/index.jsp
+++ jsp-project/target/jsp-project/index.jsp
@@ -2,12 +2,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>JSP Servlet - Hello World</title>
+        <meta charset="UTF-8">
     </head>
     <body>
-        <h1><%= "Hello from the world of JSP and Servlets" %>
-        </h1>
-        <br />
-        <a href="my-servlet">Click here to go to a Servlet</a>
+        <br>
+        <h2 align="center">Registration</h2>
+
+        <form action="register" align="center">
+            Name: <input type="text" name="name"> <br><br>
+            Email: <input type="text" name="email"> <br><br>
+            Membership :
+            <input type="radio" name="membership" value="regular" > Regular
+            <input type="radio" name="membership" value="premium" checked> Premium
+            <input type="radio" name="membership" value="elite" > Elite
+            <br><br>
+            Subscriptions:
+            <input type="checkbox" name="subs" value="news"> Newsletter
+            <input type="checkbox" name="subs" value="offers"> Offers
+            <input type="checkbox" name="subs" value="recs"> Recommendations
+            <br><br>
+            <input type="submit" value="Submit">
+            <input type="reset" >
+        </form>
+
     </body>
 </html>
